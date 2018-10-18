a068feca4d2dfd2b00d48d4687bba84ebf17815d
Abdul <abdul.ismail06@gmail.com>
2018-10-18 12:59:45 +0100

    fixed click

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,5 +1,5 @@
-// var socket = io.connect('https://abdulchat.herokuapp.com/')
-var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('https://abdulchat.herokuapp.com/')
+// var socket = io.connect('http://localhost:3000/');
 var backgroundColor = 51;
 
 setup = () => {
