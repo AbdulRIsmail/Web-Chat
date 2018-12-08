f6ee90f4c25ae24893cb7c658414c40f32b10b79
Abdul <abdul.ismail06@gmail.com>
2018-12-08 09:23:50 +0000

    fixed files

--- client/database.js
+++ client/database.js
@@ -1,23 +1,56 @@
 const mongoose = require('mongoose');
+const ObjectId = require('mongodb').ObjectID;
 const colors = require('colors');
 
 mongoose.connect('mongodb://localhost:27017/room', {useNewUrlParser: true}, (err) => {
   if (err) throw err;
-  console.log(colors.red('Successfully Connected to Database'));
+  console.log('Successfully Connected to Database');
+})
+
+const chatSchema = mongoose.Schema({
+  username: String,
+  post: String,
 })
 
 const roomSchema = mongoose.Schema({
   roomName: String,
   roomPin: String,
+  posts: [chatSchema]
 })
 
 const roomInfo = mongoose.model('roomInfo', roomSchema);
 
-// createRoom = async (roomName, roomPin) => {
-//   await roomInfo({
-//     roomName: roomName,
-//     roomPin: roomPin
-//   }).save();
-// };
+createRoom = async (roomName, roomPin) => {
+  await roomInfo({
+    roomName: roomName,
+    roomPin: roomPin
+  }).save();
+};
+
+comment = async (roomName, username, post) => {
+  await roomInfo.updateOne({roomName: roomName},
+    {$push:
+      {posts:
+        {username: username, post: post}
+      }
+    })
+}
+
+removeComment = async (roomId, commentId) => {
+  const comment = await roomInfo.updateOne({_id: roomId}, {  $pull: {posts: {_id: commentId} }});
+
+  if (comment.nModified == 0) {
+    console.log('comment already removed!');
+    return false
+  } else {
+    console.log('removing comment');
+    return true
+  }
+
+  return false
+}
 
 module.exports = roomInfo;
+module.exports.comment = comment;
+module.exports.removeComment = removeComment;
+module.exports.createRoom = createRoom;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const colors = require('colors');
 const socket = require('socket.io');
-const token = require('./client/tokenHelper');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const roomSchema = require('./client/database');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -14,7 +15,7 @@ server = app.listen(port, () => {
 var io = socket(server);
 
 io.on('connection', (socket) => {
-  console.log('New Socket Connection', colors.yellow(socket.id))
+  console.log('New Socket Connection', colors.yellow(socket.id));
 
   // Handle Chat Event
   socket.on('chat', (data) => {
@@ -46,28 +47,133 @@ io.on('connection', (socket) => {
 // Static Files - make them global and work within express
 app.use(express.static('public/views'));
 app.use(express.static('public/'));
-app.set("view engine", "html")
+app.set("view engine", "html");
 
-
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+
 app.post('/api/createroom', (req, res) => {
-  // res.sendFile(__dirname + "/test.html");
-
   (async () => {
-    console.log('valled')
-    const doc = await roomSchema.findOne({roomName: req.body.roomName}, {})
-    if (doc){
-      res.send('exist')
+    const roomName = req.body.roomName;
+    const roomPin = req.body.roomPin;
 
-    }else {
-      res.send('not exit')
+    if (roomName == '' && roomPin == '') {
+      res.send('Please enter room name and room pin!')
+      return
     }
 
+    if (roomName == '') {
+      res.send('room name is blank!')
+      return
+    }
+
+    if (roomPin == '') {
+      res.send('room pin is blank!')
+      return
+    }
+
+    if (/[^a-zA-Z0-9]/.test(roomName) && /[^a-zA-Z0-9]/.test(roomPin)) {
+      res.send('Room name and Pin may only contain alphanumeric characters (letters or numbers).')
+    }
+
+    if (/[^a-zA-Z0-9]/.test(roomName)) {
+      res.send('Room name may only contain alphanumeric characters (letters or numbers).')
+      return
+    }
+
+    if (/[^a-zA-Z0-9]/.test(roomPin)) {
+      res.send('Room pin may only contain alphanumeric characters (letters or numbers).')
+      return
+    }
+
+    const doc = await roomSchema.findOne({roomName: roomName});
+
+    if (doc) {
+      res.send('room exists');
+      return
+    } else {
+      // res.send('created room')
+      await roomSchema.createRoom(roomName, roomPin);
+      var token = jwt.sign({roomId: roomId, roomName: roomName, roomPin: roomPin}, 'supersecret');
+      res.send(token)
+    }
 
   })()
-
-
-  // res.send(req.body)
 })
+
+
+app.post('/api/joinroom', (req, res) => {
+  (async () => {
+    const roomName = req.body.roomName;
+    const roomPin = req.body.roomPin;
+
+    if (roomName == '' || roomPin == '') {
+      res.send('Missing Fields!')
+      return
+    }
+
+    const doc = await roomSchema.findOne({roomName: roomName});
+
+    // checks if room exists
+    if (doc) {
+
+      // checks if the pin matches the room
+      if (doc.roomPin == roomPin) {
+        res.send('Joining Room!');
+        const token = jwt.sign({roomId: doc._id, roomName: roomName, roomPin: roomPin}, 'supersecret');
+        console.log(token);
+      } else {
+        res.send('Invalid Pin!');
+      }
+    } else {
+      res.send('Room does not exist!')
+    }
+
+  })()
+})
+
+app.post('/api/dashboard', (req, res) => {
+  const token = req.body.token;
+  jwt.verify(token, 'supersecret', (err, decoded) => {
+    if (!err) {
+      res.send('validated!')
+      console.log(decoded);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post('/api/comment', (req, res) => {
+  const token = req.body.token;
+  const username = req.body.username;
+  const post = req.body.post;
+
+  jwt.verify(token, 'supersecret', (err, decoded) => {
+    if (!err) {
+      roomSchema.comment(decoded.roomName, username, post);
+      res.send('Posted Comment!')
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post('/api/removeComment', (req, res) => {
+  const token = req.body.token;
+  const commentId = req.body.commentId;
+
+  jwt.verify(token, 'supersecret', (err, decoded) => {
+    if (!err) {
+      roomSchema.removeComment(decoded.roomId, commentId);
+      res.end();
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+
+
+
+//
